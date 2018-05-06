f0894bc4de52e4af04224fe5cf6c88e72811eefb
Sunil Aggarwal <sunil.uiet@gmail.com>
2018-05-06 16:19:22 +0530

    Added Controller file

---
+++ GitHubVisualStudio3/Controllers/TestTwoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GitHubVisualStudio3.Controllers
+{
+    public class TestTwoController : Controller
+    {
+        // GET: TestTwo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
